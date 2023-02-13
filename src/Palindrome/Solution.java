2b13a4601e5e9f672636ee014d99557af0fe7781
Sai Prasanth K <93113585+sai-prasanth-k@users.noreply.github.com>
2023-02-13 20:27:18 +0530

    Palindrome

---
+++ src/Palindrome/Solution.java
@@ -0,0 +1,22 @@
+package Palindrome;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x ==0) return true;
+        if(x <0 || x %10 ==0) return false;
+
+        int reversed_num = 0;
+
+        while(x>reversed_num){
+            int pop = x % 10 ;
+            x /=10;
+
+            reversed_num = (reversed_num * 10) + pop;
+        }
+        if(x == reversed_num || x == reversed_num /10 ) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+}

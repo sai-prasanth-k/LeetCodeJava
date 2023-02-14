1f9069471a7445bbe6770004a3414f21d893b4ef
Sai Prasanth K <93113585+sai-prasanth-k@users.noreply.github.com>
2023-02-14 07:27:37 +0530

    Reversed Linked List

---
+++ src/ReversedLinkedList/Solution.java
@@ -0,0 +1,25 @@
+package ReversedLinkedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode output = null;
+        while(head != null){
+            ListNode tmp = head.next;
+            head.next = output;
+            output = head;
+            head = tmp;
+        }
+
+        return output;
+    }
+}

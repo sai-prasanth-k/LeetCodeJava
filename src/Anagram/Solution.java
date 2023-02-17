6e562d39f4141657dda9d8c152f6672171b37d20
Sai Prasanth K <93113585+sai-prasanth-k@users.noreply.github.com>
2023-02-17 08:44:09 +0530

    Anagram

---
+++ src/Anagram/Solution.java
@@ -0,0 +1,18 @@
+package Anagram;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int sLen = s.length();
+        int tLen = t.length();
+        if(sLen != tLen) return false;
+        int[] char_count = new int[26];
+        for(int i =0; i<sLen;i++){
+            char_count[s.charAt(i)-'a']++;
+            char_count[t.charAt(i)-'a']--;
+        }
+        for(int count : char_count){
+            if(count !=0) return false;
+        }
+        return true;
+    }
+}

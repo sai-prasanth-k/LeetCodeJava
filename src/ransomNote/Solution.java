81336a703f0fbe84ac60bf166c7f1f15f7522723
Sai Prasanth K <93113585+sai-prasanth-k@users.noreply.github.com>
2023-02-18 11:55:08 +0530

    Ransom Note

---
+++ src/ransomNote/Solution.java
@@ -0,0 +1,21 @@
+package ransomNote;
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int rLen = ransomNote.length();
+        int mLen = magazine.length();
+        if(rLen > mLen || rLen == 0) return false;
+        char m[] = magazine.toCharArray();
+        char r[] = ransomNote.toCharArray();
+
+        for(char rn : r){
+            if(magazine.contains(Character.toString(rn))) {
+                magazine = magazine.replaceFirst(Character.toString(rn),"");
+            } else {
+                return false;
+            }
+
+        }
+        return true;
+    }
+}

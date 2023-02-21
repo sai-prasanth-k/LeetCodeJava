48344f987abc58470ce056d489e1786ed862a325
Sai Prasanth K <93113585+sai-prasanth-k@users.noreply.github.com>
2023-02-21 18:05:02 +0530

    Updated

--- src/Arrays/twoSums/HashMap.java
+++ src/Arrays/twoSums/HashMap.java
@@ -19,3 +19,20 @@ public class HashMap {
             return result;
         }
 }
+//class Solution {
+//    public int[] twoSum(int[] nums, int target) {
+//        int result[] = new int[2];
+//        int numLen = nums.length;
+//        HashMap<Integer, Integer> map = new HashMap();
+//        for(int i =0; i< numLen;i++){
+//            int complement = target - nums[i];
+//            if(map.containsKey(complement)){
+//                result[0] = map.get(complement);
+//                result[1] = i;
+//            }
+//            map.put(nums[i],i);
+//        }
+//        return result;
+//    }
+//}
+

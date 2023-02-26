d9a4a2c090912e067fc7cef371e5b6e4ac8d9fd5
Sai Prasanth K <93113585+sai-prasanth-k@users.noreply.github.com>
2023-02-26 07:42:21 +0530

    How many numbers are smaller than current number in an array

---
+++ src/Arrays/HowManyNoAreSmallerThanCurrentNumber/Solution.java
@@ -0,0 +1,18 @@
+package Arrays.HowManyNoAreSmallerThanCurrentNumber;
+
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] res = new int[nums.length];
+        int count =0;
+        for(int i = 0 ;i<nums.length;i++){
+            for(int j = 0 ; j< nums.length;j++){
+                if(nums[i] > nums[j]){
+                    count+=1;
+                }
+            }
+            res[i] = count;
+            count = 0;
+        }
+        return res;
+    }
+}

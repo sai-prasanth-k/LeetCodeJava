b27a516613fad972e0f0132d50bc90d863c768b1
Sai Prasanth K <93113585+sai-prasanth-k@users.noreply.github.com>
2023-03-06 22:51:21 +0530

    Single Number

---
+++ src/Arrays/SingleNumber/Solution.java
@@ -0,0 +1,15 @@
+package Arrays.SingleNumber;
+
+import java.util.*;
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0 ; i < nums.length-1;i++){
+            if(nums[i] != nums[i+1]){
+                return nums[i];
+            }
+            i++;
+        }
+        return nums[nums.length -1];
+    }
+}

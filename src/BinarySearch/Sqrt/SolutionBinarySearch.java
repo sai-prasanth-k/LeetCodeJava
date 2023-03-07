7aa870c74c0b84d092b2a17f42cda0f5febe217f
Sai Prasanth K <93113585+sai-prasanth-k@users.noreply.github.com>
2023-03-07 22:59:54 +0530

    Binary Search Solution for Square root

--- src/BinarySearch/Sqrt/SolutionBinarySearch.java
+++ src/BinarySearch/Sqrt/SolutionBinarySearch.java
@@ -2,13 +2,18 @@ package BinarySearch.Sqrt;
 
 public class SolutionBinarySearch {
         public int mySqrt(int x) {
-            if(x==0) return 0;
-            int low = 1,high = x,ans =0;
+            int low = 1 ;
+            int high = x;
+            int ans = 0;
             while(low<=high){
-                int mid =low + (high-low)/2;
-                if(x/mid==mid)  return mid;
-                else if(x/mid<mid)   high=mid-1;
-                else {low = mid+1; ans = mid;}
+                int mid = low +(high - low)/2;
+                if(x/mid == mid) return mid;
+                if(x/mid<mid) {
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                    ans = mid;
+                }
             }
             return ans;
         }

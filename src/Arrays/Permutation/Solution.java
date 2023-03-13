c8b7f86932b35b4d4111cae2eb2c60fae44c621a
Sai Prasanth K <93113585+sai-prasanth-k@users.noreply.github.com>
2023-03-13 20:31:30 +0530

    Permutation

---
+++ src/Arrays/Permutation/Solution.java
@@ -0,0 +1,23 @@
+package Arrays.Permutation;
+
+import java.util.*;
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res= new ArrayList<>();
+        backTrack(res, new ArrayList<>(), nums);
+        return res;
+    }
+
+    public void backTrack(List<List<Integer>> res, List<Integer> tempList, int[] nums){
+        if(tempList.size() == nums.length){
+            res.add(new ArrayList<>(tempList));
+        }else {
+            for(int i = 0 ; i < nums.length;i++){
+                if(tempList.contains(nums[i])) continue;
+                tempList.add(nums[i]);
+                backTrack(res, tempList, nums);
+                tempList.remove(tempList.size() -1);
+            }
+        }
+    }
+}

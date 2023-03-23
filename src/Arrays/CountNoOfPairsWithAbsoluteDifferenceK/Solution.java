593c77666050a78880a3a9e8f66ba3bb9a8f4049
Sai Prasanth K <93113585+sai-prasanth-k@users.noreply.github.com>
2023-03-23 13:13:05 +0530

    Count Number of Pairs With Absolute Difference K

---
+++ src/Arrays/CountNoOfPairsWithAbsoluteDifferenceK/Solution.java
@@ -0,0 +1,14 @@
+package Arrays.CountNoOfPairsWithAbsoluteDifferenceK;
+
+class Solution {
+    public int countKDifference(int[] nums, int k) {
+        int count = 0 ;
+
+        for(int i = 0 ; i < nums.length ; i++){
+            for(int j = 0 ; j < nums.length ; j++){
+                if(nums[i] - nums[j] == k) count ++;
+            }
+        }
+        return count;
+    }
+}
